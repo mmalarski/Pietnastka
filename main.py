317d29b68647aadab40eb812ba04030ddaf3397a
mmalarski <malarsky.mike@gmail.com>
2021-10-27 20:01:53 +0200

    dfs some fixes

--- main.py
+++ main.py
@@ -1,22 +1,27 @@
 from copy import deepcopy
 
 DEPTH = 20
-TARGET_STATE = [[1, 2, 3, 4],
-                [5, 6, 7, 8],
-                [9, 10, 11, 12],
-                [13, 14, 15, 0]]
-INITIAL_STATE = [[1, 2, 3, 4],
-                [5, 6, 0, 7],
-                [9, 10, 11, 8],
-                [13, 14, 15, 12]]
-ORDER = "LURD"
+# TARGET_STATE = [[1, 2, 3, 4],
+#                 [5, 6, 7, 8],
+#                 [9, 10, 11, 12],
+#                 [13, 14, 15, 0]]
+TARGET_STATE = [[1, 2, 3],
+                 [4, 5, 6],
+                 [7, 8, 0]]
+# INITIAL_STATE = [[1, 2, 3, 0],
+#                  [5, 6, 7, 4],
+#                  [9, 10, 11, 8],
+#                  [13, 14, 15, 12]]
+INITIAL_STATE = [[1, 2, 3],
+                 [4, 0, 6],
+                 [7, 5, 8]]
+ORDER = "DLUR"
 EXPERIMENTAL_STATE = [[8, 15, 3, 0],
                       [5, 6, 4, 11],
                       [2, 9, 10, 12],
                       [1, 14, 7, 13]]
 
 POS = [0, 0]
-
 
 
 class Step:
@@ -106,81 +111,58 @@ def dfs():
     move_to_CL = False
     iteration_counter = 0
     debug_counter = 0
+    print('START')
+    print_board(step.board)
     while step.board != TARGET_STATE:
         # print(debug_counter)
-        print('START')
-        print_board(step.board)
         # print_board(open_list)
         # print_board(closed_list)
-        zero_pos_x, zero_pos_y = POS[0], POS[1]
         posdirs = [char for char in getPossibleDirections(step.board, ORDER)]
         iteration_counter = 0
+        print(POS[0], POS[1], posdirs)
         for direction in posdirs:
-            sym = sym_move_step(direction, step.board, zero_pos_x, zero_pos_y)  # board sym for the specific direction
+            print(direction)
+            sym = sym_move_step(direction, step.board, POS[0], POS[1])  # board sym for the specific direction
             if sym not in [b.board for b in open_list]:  # child is not on open list
                 if sym not in [b.board for b in closed_list]:  # child is not on closed list
-                    print('out')
+                    print('out', direction)
                     # step = step.move_step(posdirs[0], step.board, POS[0], POS[1])  # not on lists so we move
                     step = step.move_step(direction, step.board, POS[0], POS[1])
                     print_board(step.board)
                     open_list.append(step)  # adding current step to open_list
                     break  # getting out of for statement
-                else:  # child is on closed list
-                    posdirs.pop(0)  # we remove child's direction from direction list
-                    for i in posdirs:
-                        if sym in closed_list:
-                            posdirs.pop(i)
-                            if len(posdirs) == 0:
-                                open_list.remove(step)
-                                closed_list.append(step)
-                                step = step.parent
-                        else:
-                            break
-                    # step = step.move_step(posdirs[0], step.board, POS[0], POS[1])
-                    step = step.move_step(direction, step.board, POS[0], POS[1])
-                    open_list.append(step)
-                    break
+                # else:  # child is on closed list
+                    # posdirs.pop(0)  # we remove child's direction from direction list
+                    # for i in posdirs:
+                    #     if sym in closed_list:
+                    #         posdirs.pop(i)
+                    #         if len(posdirs) == 0:
+                    #             open_list.remove(step)
+                    #             closed_list[step] = step
+                    #             step = step.parent
+                    #             print('''
+                    #
+                    #             HHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH
+                    #             EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE
+                    #             LLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLL
+                    #             LLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLL
+                    #             OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO
+                    #
+                    #             ''')
+                    #     else:
+                    #         break
+                    # # step = step.move_step(posdirs[0], step.board, POS[0], POS[1])
+                    # step = step.move_step(direction, step.board, POS[0], POS[1])
+                    # open_list.append(step)
+                    # break
             iteration_counter = iteration_counter + 1
-            print(iteration_counter)
+            # print(iteration_counter)
         if iteration_counter == len(posdirs):
             # move_to_CL = True
             iteration_counter = 0
             open_list.remove(step)
             closed_list[step] = step
             step = step.parent
-            # continue
-        # if move_to_CL:
-        #     continue
-        else:
-            continue
-            # open_list.remove(step)
-            # closed_list[step] = step
-            # step = step.parent
-
-
-
-
-
-
-
-            # copystep = deepcopy(step)
-            # temp_x = deepcopy(zero_pos_x)
-            # temp_y = deepcopy(zero_pos_y)
-            # copystep.move_step(posdirs[0], copystep.board, temp_x, temp_y)
-            # if copystep != step.parent:
-            #     open_list.remove(step)
-            #     closed_list[step] = step
-            #     step = step.parent
-            # else:
-            #     posdirs.pop(0)
-            #     for i in posdirs:
-            #         if posdirs[0] in closed_list:
-            #             posdirs.pop()
-            #             if len(posdirs) == 0:
-            #                 return -1
-            #         else:
-            #             step.move_step(posdirs[0], state.board, zero_pos_x, zero_pos_y)
-            #             open_list.append(step)
 
 
 def a_star():
