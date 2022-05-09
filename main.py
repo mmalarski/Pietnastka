976cf5f9bcea9e5a07a76b94259ddfc467347418
L-Limeee <monikadyczka@gmail.com>
2022-05-09 12:10:04 +0200

    Fixed algorithms

--- main.py
+++ main.py
@@ -29,6 +29,15 @@ class Step:
         if previous_move is not None:
             self.all_moves.append(previous_move)
 
+    def __eq__(self, o: object) -> bool: #equals
+        if not isinstance(o, Step): #sprawdza czy to jest instancja
+            return False
+        o: Step = o
+        return all(l1 == l2 for l1, l2 in zip(o.board, self.board)) #sprawdza czy układanki są takie same, czy wszystkie pola są takie same
+
+    def __hash__(self): #funkcja hashująca, zwraca unikalny identyfikator obiektu, jeśli układanki będą takie same (jak przez funkcje equals na przykład) to hashe będą takie same
+        return hash(tuple(tuple(el for el in row) for row in self.board))
+
     @property
     def f_m(self):
         return len(self.all_moves) + distance_manhattan(self.board)
@@ -70,7 +79,7 @@ class Step:
         return new_step
 
 
-def bfs(order, variant, init_state, t_state):
+def bfs(order, init_state, t_state):
     step = Step(None, None, [], init_state)
     open_list = [step]
     closed_list = {}
@@ -78,7 +87,7 @@ def bfs(order, variant, init_state, t_state):
     start_time = t.time()
     while open_list:
         step = open_list.pop(0)
-        if closed_list.get(step.board_string) is None:
+        if step not in closed_list:
             if len(step.all_moves) > max_depth:
                 max_depth = len(step.all_moves)
             if step.board == t_state:
@@ -86,23 +95,23 @@ def bfs(order, variant, init_state, t_state):
                 return str(len(list_to_string(step.all_moves))), list_to_string(step.all_moves), len(open_list) + len(
                     closed_list), len(closed_list), max_depth, t.time() - start_time
             closed_list[step.board_string] = step.all_moves
-            for direction in get_possible_directions(step.board, order):
+            for direction in get_possible_directions(step.board, order, step.previous_move):
                 child = deepcopy(step)
                 child = child.move_step(direction, step.board, find_zero(step.board)[0], find_zero(step.board)[1])
                 if child.board == t_state:
                     # stan odwiedzone - kiedykolwiek byly na openliscie
                     return str(len(list_to_string(child.all_moves))), list_to_string(child.all_moves), len(open_list) \
                            + len(closed_list), len(closed_list), max_depth, t.time() - start_time
-                if closed_list.get(child.board_string) is None:
+                if child not in closed_list:
                     open_list.append(child)
                 elif len(closed_list.get(child.board_string)) > len(child.all_moves):
                     # optimalisation for each method all moves
                     open_list.append(child)
                     closed_list.pop(child.board_string)
-    return str(-1), list_to_string(step.all_moves), len(open_list), len(closed_list), max_depth, t.time() - start_time
+    return str(-1), list_to_string(step.all_moves), len(open_list) + len(closed_list), len(closed_list), max_depth, t.time() - start_time
 
 
-def dfs(order, variant, init_state, t_state):
+def dfs(order, init_state, t_state):
     step = Step(None, None, [], init_state)
     open_list = [step]
     closed_list = {}
@@ -110,104 +119,31 @@ def dfs(order, variant, init_state, t_state):
     start_time = t.time()
     while open_list:
         step = open_list.pop()
-        if closed_list.get(step.board_string) is None:
+       # print(sq_list_to_string(step.board))
+        if step not in closed_list:
             if len(step.all_moves) > max_depth:
                 max_depth = len(step.all_moves)
             if step.board == t_state:
                 # stan odwiedzone - kiedykolwiek byly na openliscie
-                return str(len(list_to_string(step.all_moves))), list_to_string(step.all_moves), len(open_list), len(
+                return str(len(list_to_string(step.all_moves))), list_to_string(step.all_moves), len(open_list) + len(closed_list), len(
                     closed_list), max_depth, t.time() - start_time
             closed_list[step.board_string] = step.all_moves
             if len(step.all_moves) == DEPTH:
                 continue
-            for direction in reversed(get_possible_directions(step.board, order)):
+            for direction in reversed(get_possible_directions(step.board, order, step.previous_move)):
                 child = deepcopy(step)
                 child = child.move_step(direction, step.board, find_zero(step.board)[0], find_zero(step.board)[1])
                 if child.board == t_state:
                     # stan odwiedzone - kiedykolwiek byly na openliscie
                     return str(len(list_to_string(child.all_moves))), list_to_string(child.all_moves), len(open_list) \
                            + len(closed_list), len(closed_list), max_depth, t.time() - start_time
-                if closed_list.get(child.board_string) is None:
+                if child not in closed_list:
                     open_list.append(child)
                 elif len(closed_list.get(child.board_string)) > len(child.all_moves):
                     # optimalisation for each method all moves
                     open_list.append(child)
                     closed_list.pop(child.board_string)
-    return str(-1), list_to_string(step.all_moves), len(open_list), len(closed_list), max_depth, t.time() - start_time
-
-
-def blind_algorithms(order, variant, init_state, t_state):
-    step = Step(None, None, [], init_state)
-    open_list = [step]
-    closed_list = {}
-    max_depth = 0
-    start_time = t.time()
-    while open_list:
-        if variant == "dfs":
-            step = open_list.pop()
-        elif variant == "bfs":
-            step = open_list.pop(0)
-        else:
-            return "Wrong variant"
-
-        if closed_list.get(step.board_string) is None:
-            if len(step.all_moves) > max_depth:
-                max_depth = len(step.all_moves)
-            if step.board == t_state:
-                # stan odwiedzone - kiedykolwiek byly na openliscie
-                return str(len(list_to_string(step.all_moves))), list_to_string(step.all_moves), len(closed_list) + \
-                       len(open_list), len(closed_list), max_depth, t.time() - start_time
-            closed_list[step.board_string] = step.all_moves
-            if variant == "dfs" and len(step.all_moves) == DEPTH:
-                continue
-            for direction in reversed(get_possible_directions(step.board, order)): # reversed order depending on method
-                child = deepcopy(step)
-                child = child.move_step(direction, step.board, find_zero(step.board)[0], find_zero(step.board)[1])
-                #is target state return
-                if closed_list.get(child.board_string) is None:
-                    open_list.append(child)
-                elif len(closed_list.get(child.board_string)) > len(child.all_moves):
-                    # optimalisation for each method all moves
-                    open_list.append(child)
-                    closed_list.pop(child.board_string)
-    return str(-1), list_to_string(step.all_moves), len(closed_list) + len(open_list), len(closed_list), max_depth, t.time() - start_time
-
-
-def blind_algorithms(order, variant, init_state, t_state):
-    step = Step(None, None, [], init_state)
-    open_list = [step]
-    closed_list = {}
-    max_depth = 0
-    start_time = t.time()
-    while open_list:
-        if variant == "dfs":
-            step = open_list.pop()
-        elif variant == "bfs":
-            step = open_list.pop(0)
-        else:
-            return "Wrong variant"
-
-        if closed_list.get(step.board_string) is None:
-            if len(step.all_moves) > max_depth:
-                max_depth = len(step.all_moves)
-            if step.board == t_state:
-                # stan odwiedzone - kiedykolwiek byly na openliscie
-                return str(len(list_to_string(step.all_moves))), list_to_string(step.all_moves), len(closed_list) + \
-                       len(open_list), len(closed_list), max_depth, t.time() - start_time
-            closed_list[step.board_string] = step.all_moves
-            if variant == "dfs" and len(step.all_moves) == DEPTH:
-                continue
-            for direction in reversed(get_possible_directions(step.board, order)): # reversed order depending on method
-                child = deepcopy(step)
-                child = child.move_step(direction, step.board, find_zero(step.board)[0], find_zero(step.board)[1])
-                #is target state return
-                if closed_list.get(child.board_string) is None:
-                    open_list.append(child)
-                elif len(closed_list.get(child.board_string)) > len(child.all_moves):
-                    # optimalisation for each method all moves
-                    open_list.append(child)
-                    closed_list.pop(child.board_string)
-    return str(-1), list_to_string(step.all_moves), len(closed_list) + len(open_list), len(closed_list), max_depth, t.time() - start_time
+    return str(-1), list_to_string(step.all_moves), len(open_list) + len(closed_list), len(closed_list), max_depth, t.time() - start_time
 
 
 def a_star(variant, init_state, t_state):
@@ -231,9 +167,9 @@ def a_star(variant, init_state, t_state):
         if len(step.all_moves) > max_depth:
             max_depth = len(step.all_moves)
         if step.board == t_state:
-            return str(len(list_to_string(step.all_moves))), list_to_string(step.all_moves), len(open_list), len(
+            return str(len(list_to_string(step.all_moves))), list_to_string(step.all_moves), len(open_list) + len(closed_list), len(
                 closed_list), max_depth, t.time() - start_time
-        for direction in get_possible_directions(step.board, "LURD"):
+        for direction in get_possible_directions(step.board, "LURD", step.previous_move):
             # check if is target
             child = deepcopy(step)
             child = child.move_step(direction, step.board, find_zero(step.board)[0], find_zero(step.board)[1])
@@ -292,55 +228,67 @@ def distance_manhattan(board):
     return distance_result
 
 
-def get_possible_directions(board, order):
+def get_opposite_direction(direction):
+    if direction == "U":
+        return "D"
+    elif direction == "D":
+        return "U"
+    elif direction == "L":
+        return "R"
+    elif direction == "R":
+        return "L"
+
+
+def get_possible_directions(board, order, lastDir):
     row, col = find_zero(board)
     size_x, size_y = len(board[0]), len(board)
-
+    lastDir = get_opposite_direction(lastDir)
     # CORNERS
     # top left corner
     if row == 0 and col == 0:
-        result = [char for char in order if char not in "UL"]
-        return result[0], result[1]
+        result = [char for char in order if char not in f"UL{lastDir}"]
+        return result[0]
 
     # top right corner
     elif row == 0 and col == size_x - 1:
-        result = [char for char in order if char not in "UR"]
-        return result[0], result[1]
+        result = [char for char in order if char not in f"UR{lastDir}"]
+        return result[0]
 
     # bottom right corner
     elif row == size_y - 1 and col == size_x - 1:
-        result = [char for char in order if char not in "DR"]
-        return result[0], result[1]
+        result = [char for char in order if char not in f"DR{lastDir}"]
+        return result[0]
 
     # bottom left corner
     elif row == size_y - 1 and col == 0:
-        result = [char for char in order if char not in "LD"]
-        return result[0], result[1]
+        result = [char for char in order if char not in f"LD{lastDir}"]
+        return result[0]
 
     # EDGES
     # top edge
     elif row == 0:
-        result = [char for char in order if char not in "U"]
-        return result[0], result[1], result[2]
+        result = [char for char in order if char not in f"U{lastDir}"]
+        return result[0], result[1]
 
     # right edge
     elif col == size_x - 1:
-        result = [char for char in order if char not in "R"]
-        return result[0], result[1], result[2]
+        result = [char for char in order if char not in f"R{lastDir}"]
+        return result[0], result[1]
 
     # bottom edge
     elif row == size_y - 1:
-        result = [char for char in order if char not in "D"]
-        return result[0], result[1], result[2]
+        result = [char for char in order if char not in f"D{lastDir}"]
+        return result[0], result[1],
 
     # left edge
     elif col == 0:
-        result = [char for char in order if char not in "L"]
-        return result[0], result[1], result[2]
+        result = [char for char in order if char not in f"L{lastDir}"]
+        return result[0], result[1]
 
     # MIDDLE
     else:
-        return order[0], order[1], order[2], order[3]
+        result = [char for char in order if char not in lastDir]
+        return result[0], result[1], result[2]
 
 
 def convert_to_boards(text):
@@ -363,61 +311,61 @@ def convert_to_boards(text):
 
 if __name__ == '__main__':
     # [0]name.py [1]algorithm [2]ORDER/variant [3]initial state [4]output file solution [5]output file with statistics
-    if len(sys.argv) == 6:
-        file = open(f"{sys.argv[3]}")
-        initial_state, target_state = convert_to_boards(file.readlines())
-        file.close()
-        solution_file = open(sys.argv[4], "w")
-        statistics_file = open(sys.argv[5], "w")
-
-        if sys.argv[1] == "bfs" or sys.argv[1] == "dfs":
-            if sys.argv[2] not in ["RDUL", "RDLU", "DRUL", "DRUL", "DRLU", "LUDR", "LURD", "ULDR", "ULRD"]:
-                print("Podaj odpowiedni porzadek przeszukiwania (ORDER)")
-            else:
-                if sys.argv[1] == "bfs":
-                    length, solution, ol_size, cl_size, depth, time = bfs(sys.argv[2], sys.argv[1], initial_state,
-                                                                          target_state)
-                elif sys.argv[1] == "dfs":
-                    length, solution, ol_size, cl_size, depth, time = dfs(sys.argv[2], sys.argv[1], initial_state,
-                                                                          target_state)
-                solution_file.writelines([length, '\n', solution]) if length != "-1" else solution_file.writelines(
-                    [length])
-                statistics_file.write(length)
-                if length != -1:
-                    statistics_file.writelines(['\n', str(ol_size),
-                                                '\n', str(cl_size),
-                                                '\n', str(depth),
-                                                '\n', str(round(time * 1000, 3))])
-        elif sys.argv[1] == "astr":
-            if sys.argv[2] not in ["manh", "hamm"]:
-                print("Podaj odpowiedni wariant metody A*")
-            else:
-                length, solution, ol_size, cl_size, depth, time = a_star(sys.argv[2], initial_state, target_state)
-                solution_file.writelines([length, '\n', solution])
-                statistics_file.writelines([length,
-                                            '\n', str(ol_size),
-                                            '\n', str(cl_size),
-                                            '\n', str(depth),
-                                            '\n', str(round(time * 1000, 3))])
-        else:
-            print(f"Podano bledna nazwe algorytmu: {sys.argv[1]}")
-
-        solution_file.close()
-        statistics_file.close()
-    else:
-        print("Podaj poprawna ilosc argumentow (5)")
+    # if len(sys.argv) == 6:
+    #     file = open(f"{sys.argv[3]}")
+    #     initial_state, target_state = convert_to_boards(file.readlines())
+    #     file.close()
+    #     solution_file = open(sys.argv[4], "w")
+    #     statistics_file = open(sys.argv[5], "w")
     #
-    # INITIAL_STATE = [
-    #     [0, 2, 3, 4],
-    #     [1, 5, 6, 8],
-    #     [9, 10, 7, 12],
-    #     [13, 14, 11, 15]
-    # ]
-    # TARGET_STATE = [
-    #     [1, 2, 3, 4],
-    #     [5, 6, 7, 8],
-    #     [9, 10, 11, 12],
-    #     [13, 14, 15, 0]
-    # ]
+    #     if sys.argv[1] == "bfs" or sys.argv[1] == "dfs":
+    #         if sys.argv[2] not in ["RDUL", "RDLU", "DRUL", "DRUL", "DRLU", "LUDR", "LURD", "ULDR", "ULRD"]:
+    #             print("Podaj odpowiedni porzadek przeszukiwania (ORDER)")
+    #         else:
+    #             if sys.argv[1] == "bfs":
+    #                 length, solution, ol_size, cl_size, depth, time = bfs(sys.argv[2], sys.argv[1], initial_state,
+    #                                                                       target_state)
+    #             elif sys.argv[1] == "dfs":
+    #                 length, solution, ol_size, cl_size, depth, time = dfs(sys.argv[2], sys.argv[1], initial_state,
+    #                                                                       target_state)
+    #             solution_file.writelines([length, '\n', solution]) if length != "-1" else solution_file.writelines(
+    #                 [length])
+    #             statistics_file.write(length)
+    #             if length != -1:
+    #                 statistics_file.writelines(['\n', str(ol_size),
+    #                                             '\n', str(cl_size),
+    #                                             '\n', str(depth),
+    #                                             '\n', str(round(time * 1000, 3))])
+    #     elif sys.argv[1] == "astr":
+    #         if sys.argv[2] not in ["manh", "hamm"]:
+    #             print("Podaj odpowiedni wariant metody A*")
+    #         else:
+    #             length, solution, ol_size, cl_size, depth, time = a_star(sys.argv[2], initial_state, target_state)
+    #             solution_file.writelines([length, '\n', solution])
+    #             statistics_file.writelines([length,
+    #                                         '\n', str(ol_size),
+    #                                         '\n', str(cl_size),
+    #                                         '\n', str(depth),
+    #                                         '\n', str(round(time * 1000, 3))])
+    #     else:
+    #         print(f"Podano bledna nazwe algorytmu: {sys.argv[1]}")
     #
-    # print(a_star("manh", INITIAL_STATE, TARGET_STATE))
+    #     solution_file.close()
+    #     statistics_file.close()
+    # else:
+    #     print("Podaj poprawna ilosc argumentow (5)")
+    #
+    INITIAL_STATE = [
+        [1, 2, 3, 0],
+        [5, 7, 8, 4],
+        [9, 6, 10, 12],
+        [13, 14, 11, 15]
+    ]
+    TARGET_STATE = [
+        [1, 2, 3, 4],
+        [5, 6, 7, 8],
+        [9, 10, 11, 12],
+        [13, 14, 15, 0]
+    ]
+
+    print(dfs("LUDR", INITIAL_STATE, TARGET_STATE))
